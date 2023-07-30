862ca54f86e2573ec5dee30e98eb962354933bc0
MaxKorovchenko <max.korovchenko@gmail.com>
2023-07-30 10:41:02 +0300

    add home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
+const title = {
+  fontSize: 60,
+  color: "white",
+  textShadow: "2px 2px  5px black",
+  textAlign: "center",
+  marginTop: 100,
+};
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <h1 style={title}>Welcome to the Posts App</h1>;
 };
 
 export default HomePage;
