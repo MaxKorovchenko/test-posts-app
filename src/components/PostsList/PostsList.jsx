8f277dafea854fad26ce0c75c46e2f01288432ba
MaxKorovchenko <max.korovchenko@gmail.com>
2023-07-30 11:09:22 +0300

    add load more button

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -5,14 +5,24 @@ import { PostsListItem } from "../PostsListItem/PostsListItem";
 
 import styles from "./PostsList.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const PostsList = () => {
+  const [page, setPage] = useState(1);
+
   const posts = useSelector(selectItems);
+  const visiblePosts = posts.slice(0, page * 25);
+
+  const isBtnHidden = page * 25 >= posts.length;
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <div className={styles.wrapper}>
       <ul>
-        {posts.map(({ id, ...otherProps }) => (
+        {visiblePosts.map(({ id, ...otherProps }) => (
           <li key={id} className={styles.item}>
             <Link to={`${id}`}>
               <PostsListItem {...otherProps} />
@@ -20,6 +30,12 @@ export const PostsList = () => {
           </li>
         ))}
       </ul>
+
+      {!isBtnHidden && (
+        <button className={styles.btn} type="button" onClick={handleLoadMore}>
+          Load More
+        </button>
+      )}
     </div>
   );
 };
